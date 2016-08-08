03af3538a9cb0848e96378af38876a33fec91506
Julio Quevedo <juliovquevedo@gmail.com>
2016-08-08 01:44:50 -0400

    added mouseenter and mouseleave functionality

--- client/main.js
+++ client/main.js
@@ -26,5 +26,11 @@ Template.images.helpers({images:img_data});
 Template.images.events({
 	'click .js-image': function(event) {
 		$(event.target).css("width", "50px");
+	},
+	'mouseenter .js-image': function(event) {
+		$(event.target).css("width", "100px");
+	},
+	'mouseleave .js-image': function(event) {
+		$(event.target).css("width", "100%");
 	}
 });
