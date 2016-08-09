3dee928f9da4cacfd7aaabb2b722dc7e8529c698
Julio Quevedo <juliovquevedo@gmail.com>
2016-08-09 14:23:23 -0400

    added second container on the left to be basket of all pictures

--- client/main.js
+++ client/main.js
@@ -1,23 +1,28 @@
 var img_data = [
 {
 	img_src:"robert.jpg",
-	img_alt:"Robert"
+	img_alt:"Robert",
+	place: "allpics"
 },
 {
 	img_src:"john3.jpeg",
-	img_alt:"John"
+	img_alt:"John",
+	place: "allpics"
 },
 {
 	img_src:"matthew.jpeg",
-	img_alt:"Matthew"
+	img_alt:"Matthew",
+	place: "allpics"
 },
 {
 	img_src:"brooke.jpg",
-	img_alt:"Brooke"
+	img_alt:"Brooke",
+	place: "allpics"
 },
 {
 	img_src:"eric.jpeg",
-	img_alt:"Eric"
+	img_alt:"Eric",
+	place: "allpics"
 },
 ];
 
@@ -25,7 +30,7 @@ Template.images.helpers({images:img_data});
 
 Template.images.events({
 	'click .js-image': function(event) {
-		$(event.target).css("width", "50px");
+		$(event.target).css("width", "500px");
 	},
 	'mouseenter .js-image': function(event) {
 		$(event.target).css("width", "100px");
