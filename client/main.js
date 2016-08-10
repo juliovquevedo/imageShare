29024fe6a4633e428125d937ba5ba629dd94888d
Julio Quevedo <juliovquevedo@gmail.com>
2016-08-10 14:23:10 -0400

    formatted images size, and added a descprition field to the img_data objects

--- client/main.js
+++ client/main.js
@@ -2,40 +2,45 @@ var img_data = [
 {
 	img_src:"robert.jpg",
 	img_alt:"Robert",
+	description: "Pianist and Soccer Player",
 	place: "allpics"
 },
 {
 	img_src:"john3.jpeg",
 	img_alt:"John",
+	description: "Soccer Player extraordinaire",
 	place: "allpics"
 },
 {
 	img_src:"matthew.jpeg",
 	img_alt:"Matthew",
+	description: "Math magician",
 	place: "allpics"
 },
 {
 	img_src:"brooke.jpg",
 	img_alt:"Brooke",
+	description: "The princess",
 	place: "allpics"
 },
 {
 	img_src:"eric.jpeg",
 	img_alt:"Eric",
+	description: "Future physicist",
 	place: "allpics"
 },
 ];
 
-Template.images.helpers({images:img_data});
+Template.album.helpers({images:img_data});
 
-Template.images.events({
+Template.album.events({
 	'click .js-image': function(event) {
-		$(event.target).css("width", "500px");
-	},
+		$(event.target).css("width", "50px");
+	}/*,
 	'mouseenter .js-image': function(event) {
 		$(event.target).css("width", "100px");
 	},
 	'mouseleave .js-image': function(event) {
 		$(event.target).css("width", "100%");
-	}
+	}*/
 });
